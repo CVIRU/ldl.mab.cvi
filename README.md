fb503692717d8b0bd53cc805030108bd0c446334
sargdavid <sargdavid@gmail.com>
2016-12-03 18:20:10 -0500

    Source code for trunkatednormal mean estimates.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# ldl.mab.cvi
+## Project: LDL and CV Events Reduction Through PCSK9 Inhibition by mAb Tretments   
+## Author: Davit Sargsyan
+## Created: 12/03/2016
+
+## Source 1: Efficacy and Safety of Alirocumab in Reducing Lipids and Cardiovascular Events  
+
+Trial: Long-term Safety and Tolerability of Alirocumab in High Cardiovascular Risk Patients with Hypercholesterolemia Not Adequately Controlled with Their Lipid Modifying Therapy (ODYSSEY LONG TERM, Phase III)
+Follow-up Trial: Study to Evaluate the Effect of Alirocumab on the Occurrence of Cardiovascular Events in Patients Who Have Experienced an Acute Coronary Syndrome (ODYSSEY OUTCOMES).
+Compound: Alirocumab  
+Developer: Sanofi and Regeneron Pharmaceuticals  
+Patients: 2,341  
+LDL Inclusion Criteria: >= 70 mg/dL (1.8 mM)  
+Treatment(150 mg of alirocumab):Placebo at 2:1 ratio by-weekly for 78 weeks     
+Co-theraphy: maximum tolerated dose of statins  
+Primary efficacy endpoint: % change LDL at 12 weeks  
+
+Reported: Absolute level of LDL at 12 weeks with 95% C.I. 
+
+![Alt text](media/alirocumab_plot.tif?raw=true "Title")  
+
+## Source 2: Efficacy and Safety of Evolocumab in Reducing Lipids and Cardiovascular Events
+
+Trials: Open-Label Study of Long-Term Evaluation against LDL Cholesterol 1 and 2 (OSLER-1, Phase II, and OSLER-2, Phase III)  
+Compound: Evolocumab  
+Developer: Amgen  
+Patients: 1,324 in OLSER-1 + 3,141 in OLSER-2 = 4,465  
+LDL Inclusion Criteria: multiple, spesified in 12 parental studies (Table 1)  
+Treatment(140 mg of evolocumab byweekly or 420 monthly):Placebo at 2:1 ratio, for 1 year  
+Co-theraphy: multiple, spesified in 12 parental studies (Table 1) 
+Secondary endpoint: % change LDL at 24 weeks 
+
+Reported: Absolute level of LDL at 12 weeks with 95% C.I.  
+
+NOTE: Not clear how estimates were calculated and what is being reported/plotted.    
+The plot above and the associated table (Supplemental Table S1) use medians and means simultaniously.  
+Also, reported is the combination of the 2 studies above but it si unclear how the studies were combined.  
+Results need clarification. 
+
+![Alt text](media/evolocumab_plot.tif?raw=true "Title")  
---
+++ source/ldl.mab.R
@@ -0,0 +1,118 @@
+# Code: Mean of Truncated Normal Distribution
+# Created: 11/23/2016
+# Author: Davit Sargsyan
+# Source:
+# https://assessingpsyche.wordpress.com/2014/06/04/using-the-truncated-normal-distribution/
+# Data simulation is based on reported values from:
+# 1. "Efficacy and safety of Alirocumab in reducing lipids and cardiovascular events", 
+#    Robinson et al, The New England Journal of Medicine, April 2015
+#    hhttp://www.nejm.org/doi/pdf/10.1056/NEJMoa1501031
+# 2. "Efficacy and safety of Evolocumab in reducing lipids and cardiovascular events",
+#    Sabatine et al, The New England Journal of Medicine, April 2015
+#    http://news.medlive.cn/uploadfile/20150316/14264749011448.pdf
+#*****************************************************************************************
+# Data----
+# Alirocumab: mean LDL level (mg/dL) at Baseline and Week 24
+mu.ali0 <- 122.8
+sem.ali0 <- 42.7
+N.ali0 <- 1530
+
+mu.ali24 <- 48.3 
+sem.ali24 <- 0.9
+N.ali24 <- 1386
+
+# Evolocumab: median LDL level (mg/dL) at Baseline and Week 12
+# Based on Figure 1 table
+mu.evo0 <- 100*73.4/60.9
+mu.evo12 <- mu.evo0 - 73.4
+
+# Based on Supplemental Table S1 (Appendix)
+N.evo0 <- 2976
+N.evo12 <- 2871
+# NOTE: VERY BAD ESTIMATE! 
+# Assuming 95% C.I. of achieved LDL level = 4*SD 
+std.evo0 <- (150 - 97)/4
+std.evo12 <- (71 - 32)/4
+
+#*****************************************************************************************
+# Function to calculate truncated distribution mean----
+MeanNormalTruncated <- function(mu = 0,
+                                sigma = 1,
+                                a = -Inf,
+                                b = Inf){
+  mu + sigma*(dnorm((a - mu)/sigma) - dnorm((b - mu)/sigma))/
+    (pnorm((b - mu)/sigma) - pnorm((a - mu)/sigma))
+}
+
+#*****************************************************************************************
+# Calculate and plot expected values for given threshods----
+# drug <- "Alirocumab"
+# mu <- mu.ali24
+# std <- sem.ali24*sqrt(N.ali24)
+# N <- N.ali24
+
+drug <- "Evolocumab"
+mu <- mu.evo12
+std <- std.evo12
+N <- N.evo12
+
+# th <- 15
+# th <- 25
+th <- 40 
+
+x <- seq(0, 100, 0.1)
+y <- dnorm(x = x, 
+           mean = mu, 
+           sd = std)
+
+mu1 <- MeanNormalTruncated(mu = mu,
+                           sigma = std,
+                           a = 0,
+                           b = th)
+mu1
+
+mu2 <- MeanNormalTruncated(mu = mu,
+                           sigma = std,
+                           a = th,
+                           b = 100)
+mu2
+
+plot(y ~ x,
+     type = "l",
+     xlab = "LDL",
+     ylab = "Probability",
+     main = paste("Simulation of",
+                  drug,
+                  "Effect on LDL \n Mean =" ,
+                  round(mu, 2),
+                  ", SD = ",
+                  round(std, 2),
+                  ", N = ",
+                  N,
+                  ", Threshold =",
+                  th))
+polygon(x = c(0, 
+              seq(0, th, 0.1),
+              th),
+        y = c(0, 
+              dnorm(x = seq(0, th, 0.1),
+                    mean = mu,
+                    sd = std),
+              0),
+        angle = 45,
+        density = 10)
+polygon(x = c(th, 
+              seq(th, 100, 0.1),
+              100),
+        y = c(0, 
+              dnorm(x = seq(th, 100, 0.1),
+                    mean = mu,
+                    sd = std),
+              0),
+        angle = -45,
+        density = 10)
+abline(v = c(mu1, mu2),
+       lty = 2)
+text(x = c(mu1, mu2),
+     y = c(0.008, 0.012),
+     labels = round(c(mu1, mu2)))
